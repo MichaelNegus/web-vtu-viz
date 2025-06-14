d03a8fceddaac865f1435a7ac4d7ab76969a9d1e
Caleb Brown <44173285+cgcbrown@users.noreply.github.com>
2025-06-14 18:19:26 +0100

    feat: File is read

--- next-vtu-viz/src/app/wasm-app/WasmApp.tsx
+++ next-vtu-viz/src/app/wasm-app/WasmApp.tsx
@@ -1,20 +1,19 @@
-'use client';
+"use client";
 
-import {useEffect} from "react";
-import init, {import_file_path} from "./rust-vtu.js";
+import { useEffect } from "react";
+import init from "./rust-vtu.js";
 
 export type WasmAppProps = {
-  inputFiles: File[]
-}
+  inputFiles: File[];
+};
 
-export const WasmApp = (props: WasmAppProps) => {
-
+export const WasmApp: React.FC<WasmAppProps> = (props) => {
   useEffect(() => {
     (async () => {
-      await init()
-      import_file_path(props.inputFiles[0].webkitRelativePath)
-    })()
-  }, []);
+      await init();
+      // import_file_path(props.inputFiles[0].webkitRelativePath);
+    })();
+  }, [props.inputFiles]);
 
-  return <canvas id="wasm-app" width="400" height="300"/>
-}
+  return <canvas id="wasm-app" width="400" height="300" />;
+};
