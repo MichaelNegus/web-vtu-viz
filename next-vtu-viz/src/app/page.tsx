b04cdc2303f6075c410b939644d8de5473ea6ad3
Caleb Brown <44173285+cgcbrown@users.noreply.github.com>
2025-06-14 18:40:53 +0100

    feat: fix layout:

--- next-vtu-viz/src/app/page.tsx
+++ next-vtu-viz/src/app/page.tsx
@@ -1,12 +1,14 @@
-import {Header, MainView, Sidetray} from "./components/layout";
+import { Header, MainView, Sidetray } from "./components/layout";
 
 export default function Home() {
   return (
     <main className="h-screen flex">
       <Sidetray />
-      <div className="bg-background w-full">
+      <div className="bg-background w-full h-screen flex flex-col">
         <Header />
-        <MainView />
+        <div className="flex-1 overflow-y-scroll">
+          <MainView />
+        </div>
       </div>
     </main>
   );
