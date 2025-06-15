bb02a98f313316221e38b7d59844aaba2f66e1ed
joelhi <d.j.hilmersson@gmail.com>
2025-06-15 11:20:23 +0100

    Minor changes to simple renderer

--- rust-vtu/src/plugins/mesh_renderer.rs
+++ rust-vtu/src/plugins/mesh_renderer.rs
@@ -1,27 +1,20 @@
 use bevy::{
     prelude::*,
     render::{
-        mesh::MeshTag, render_resource::{AsBindGroup, ShaderRef, ShaderType}, view::NoFrustumCulling
+        mesh::MeshTag,
+        render_resource::{AsBindGroup, ShaderRef},
     },
 };
-use vtkio::Vtk;
 
-use crate::parse::vtu_vertices;
+use crate::plugins::util;
 
 pub struct CustomMeshPlugin;
 
 impl Plugin for CustomMeshPlugin {
     fn build(&self, app: &mut App) {
+        app.add_plugins(MaterialPlugin::<CustomMaterial>::default());
         app.add_systems(Startup, spawn_custom_mesh);
     }
-}
-
-const VTU_DEMO_FILE: &str = "assets/vtu/box.vtu";
-
-fn parse_vtu_pts(path: &str) -> Result<Vec<[f64; 3]>, String> {
-    let file_path = std::path::PathBuf::from(path);
-    let vtk_file = Vtk::import(file_path).map_err(|e| e.to_string())?;
-    vtu_vertices(vtk_file)
 }
 
 fn spawn_custom_mesh(
@@ -31,22 +24,13 @@ fn spawn_custom_mesh(
 ) {
     // let points = parse_vtu_pts(VTU_DEMO_FILE).expect("Failed to read pts");
 
-    let size = 50;
-    let mut points = Vec::new();
-    for i in 0..size {
-        for j in 0..size {
-            for k in 0..size {
-                let pos = Vec3::new(i as f32, j as f32, k as f32);
-                points.push(pos);
-            }
-        }   
-    }
+    let points = util::dummy_pts(50);
 
     let mesh_cube = Mesh::from(Cuboid::default());
-        let mesh_handle = meshes.add(mesh_cube);
-        let material_handle = materials.add(CustomMaterial { scale: 0.25 });
+    let mesh_handle = meshes.add(mesh_cube);
+    let material_handle = materials.add(CustomMaterial { scale: 0.25 });
 
-    for (idx, pt) in points.iter().enumerate(){
+    for (idx, pt) in points.iter().enumerate() {
         commands.spawn((
             Mesh3d(mesh_handle.clone()),
             MeshMaterial3d(material_handle.clone()),
@@ -54,7 +38,6 @@ fn spawn_custom_mesh(
             Transform::from_xyz(pt[0], pt[1], pt[2]),
         ));
     }
-        
 }
 
 const SHADER_ASSET_PATH: &str = "shaders/instance_shader.wgsl";
