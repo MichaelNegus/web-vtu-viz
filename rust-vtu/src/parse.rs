ebd0582437fdbceacb74b08d2eae15637a147ec8
joelhi <d.j.hilmersson@gmail.com>
2025-06-15 11:22:45 +0100

    Utility parsing methods

--- rust-vtu/src/parse.rs
+++ rust-vtu/src/parse.rs
@@ -48,3 +48,53 @@ fn get_points(points: IOBuffer) -> Result<Vec<[f64; 3]>, String> {
         point_triples.push([*x.unwrap(), *y.unwrap(), *z.unwrap()]);
     }
 }
+
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+/// Parses a CSV-like file into Vec<Vec<f32>>
+pub fn parse_csv_to_vec_f32<P: AsRef<Path>>(filename: P) -> Result<Vec<Vec<f32>>, String> {
+    let file = File::open(filename).map_err(|e| e.to_string())?;
+    let reader = io::BufReader::new(file);
+
+    let mut result = Vec::new();
+
+    for line in reader.lines() {
+        let line = line.map_err(|e| e.to_string())?; // Read the line
+        let values = line
+            .split(',')
+            .map(str::trim)
+            .filter(|s| !s.is_empty())
+            .map(|s| s.parse::<f32>())
+            .collect::<Result<Vec<_>, _>>()
+            .map_err(|e| e.to_string())?;
+
+        result.push(values);
+    }
+
+    Ok(result)
+}
+
+/// Parses a CSV-like file into Vec<Vec<f32>>
+pub fn parse_csv_to_vec_i32<P: AsRef<Path>>(filename: P) -> Result<Vec<Vec<usize>>, String> {
+    let file = File::open(filename).map_err(|e| e.to_string())?;
+    let reader = io::BufReader::new(file);
+
+    let mut result = Vec::new();
+
+    for line in reader.lines() {
+        let line = line.map_err(|e| e.to_string())?; // Read the line
+        let values = line
+            .split(',')
+            .map(str::trim)
+            .filter(|s| !s.is_empty())
+            .map(|s| s.parse::<usize>())
+            .collect::<Result<Vec<_>, _>>()
+            .map_err(|e| e.to_string())?;
+
+        result.push(values);
+    }
+
+    Ok(result)
+}
